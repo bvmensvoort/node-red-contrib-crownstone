5259acf045154fceb4c5d2206279de9c6b72bcd3
bvmensvoort <info@bvmensvoort.nl>
2020-05-02 19:14:51 +0200

    Rename sha256sum file to fix build

--- package.sh
+++ package.sh
@@ -26,7 +26,7 @@ cp -r node_modules ./package
 tar --create --gzip --file=${TARFILE} ./package
 
 echo "-- Show SHA265 checksum of package"
-shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sums
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat SHA256SUMS
 
 rm -rf SHA256SUMS package
